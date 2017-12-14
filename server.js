ed260ad456083c81a962e494d536b9adab462d3b
Kevin Hall <hallk@usc.edu>
2017-12-13 18:35:00 -0800

    Refactor API with Underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,13 +21,8 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id);
-  var matchedTodo;
+  var matchedTodo = _.findWhere(todos, {id: todoId});
   
-  todos.forEach(function(todo) {
-    if (todo.id === todoId) {
-      matchedTodo = todo;
-    }
-  });
   if (matchedTodo) {
     res.json(matchedTodo);
   } else {
@@ -36,13 +32,17 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
+  
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  
+  body.description = body.description.trim();
   body.id = todoNextId++;
   
   todos.push(body);
-  
-  //res.send(JSON.stringify(body));
-  res.send(JSON.stringify(todos));
+  res.json(body);
 });
 
 app.listen(PORT, function() {
