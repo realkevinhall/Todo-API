ac954578a976384d49381ba4806cb3c6f90d31ea
Kevin Hall <hallk@usc.edu>
2017-12-13 17:45:08 -0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-  id: 1,
-  description: 'Meet Mom for lunch',
-  completed: false
-}, {
-  id: 2,
-  description: 'Go to market',
-  completed: false
-}, {
-  id: 3,
-  description: 'Pack clothes',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
 
 // GET /todos
-// GET /todos/id
-
 app.get('/todos', function(req, res) {
   res.json(todos);
 });
-
+// GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id);
   var matchedTodo;
@@ -41,8 +32,17 @@ app.get('/todos/:id', function(req, res) {
   } else {
     res.status(404).send();  
   }
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+  var body = req.body;
+  body.id = todoNextId++;
   
-//  res.send('Asking for todo with id of ' + req.params.id);
+  todos.push(body);
+  
+  //res.send(JSON.stringify(body));
+  res.send(JSON.stringify(todos));
 });
 
 app.listen(PORT, function() {
