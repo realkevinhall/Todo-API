d4ab5ff5e623b6a97adf7185ac8a8ca4943d3945
Kevin Hall <hallk@usc.edu>
2017-12-15 12:52:58 -0800

    Add Todo model and POST/todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -45,17 +46,25 @@ app.get('/todos/:id', function(req, res) {
 // POST /todos
 app.post('/todos', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed');
-  
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return res.status(400).send();
-  }
-  
-  body.description = body.description.trim();
-  body.id = todoNextId++;
-  
-  todos.push(body);
-  res.json(body);
+  db.todo.create(body).then(function(todo) {
+    res.json(todo.toJSON());
+  }, function (e) {
+    res.status(400).json(e);
+  })
 });
+//  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+//    return res.status(400).send();
+//  }
+//  
+//  body.description = body.description.trim();
+//  body.id = todoNextId++;
+//  
+//  todos.push(body);
+//  res.json(body);
+//});
+
+
+
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
@@ -94,6 +103,8 @@ app.put('/todos/:id', function(req, res) {
   res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-  console.log('Express listening on port ' + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log('Express listening on port ' + PORT);
+  });
 });
