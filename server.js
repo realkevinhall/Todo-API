fde2ee511ddb0c0a813f5c184b4e0856e0f38fc0
Kevin Hall <hallk@usc.edu>
2017-12-15 19:47:39 -0800

    Convert email to lower case

--- server.js
+++ server.js
@@ -106,6 +106,15 @@ app.put('/todos/:id', function(req, res) {
     res.status(500).send();
   });
 });
+// POST /users
+app.post('/users', function (req, res) {
+  var body = _.pick(req.body, 'email', 'password');
+  db.user.create(body).then(function(user) {
+    res.json(user.toJSON());
+  }, function (e) {
+    res.status(400).json(e);
+  });
+});
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
